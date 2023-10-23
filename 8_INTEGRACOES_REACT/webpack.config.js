a219cb5cdad148f1e19cd44ac47c6e1a912eb577
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-10-23 08:42:07 -0300

    8_INTEGRACOES_REACT

---
+++ 8_INTEGRACOES_REACT/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: '/'
+    },
+    mode: 'development',
+    devServer: {
+        port: '3000',
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+            filname: 'index.html'
+        })
+    ]
+}
